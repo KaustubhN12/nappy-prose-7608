7300c9877b30b5619e22851f5ae9d5737f7a6882
Kaustubh Nawade <112754493+KaustubhN12@users.noreply.github.com>
2023-02-23 00:55:23 +0530

    day-2

--- glam_up/src/App.js
+++ glam_up/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Components/Footer/Footer'
-import Home from './Pages/Home';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/home';
 function App() {
   return (
     <div className="App">
