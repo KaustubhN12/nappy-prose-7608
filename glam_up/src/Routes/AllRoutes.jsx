36bc73740b877e37627a072f65f732e5dcb81fa1
Kaustubh Nawade <112754493+KaustubhN12@users.noreply.github.com>
2023-02-25 01:06:58 +0530

    !

--- glam_up/src/Routes/AllRoutes.jsx
+++ glam_up/src/Routes/AllRoutes.jsx
@@ -2,12 +2,12 @@ import {Routes, Route} from "react-router-dom";
 import AddProduct from "../Admin/AddProduct";
 import RemoveProduct from "../Admin/RemoveProduct";
 
-// import CartDrawer from "../CartPage/Cart";
-// import SignIn from "../SignInPage/SignIn";
-// import Payment from "../Payment/Payment";
-// import Face from "../Face/Face";
-// import Thankx from "../Payment/Thankx";
-// import LogInComponent from "../SignInPage/Login";
+import CartDrawer from "../CartPage/Cart";
+import SignIn from "../SignInPage/SignIn";
+import Payment from "../Payment/Payment";
+import Face from "../Face/Face";
+import Thankx from "../Payment/Thankx";
+import LogInComponent from "../SignInPage/Login";
 import Admin from "../Pages/Admin";
 import Home from "../Pages/home";
 
@@ -16,12 +16,12 @@ const AllRoutes =()=>{
     return (
        <Routes>
          <Route path="/"  element={< Home />}  ></Route>
-         {/* <Route path="/cart"  element={< CartDrawer />}   ></Route>
+         <Route path="/cart"  element={< CartDrawer />}   ></Route>
          <Route path="/signIn"  element={< SignIn />}    ></Route>
          <Route path="/payment"  element={< Payment />}  ></Route>
          <Route path="/face" element={<Face />} ></Route>
          <Route path="/thanks" element={<Thankx />} ></Route>
-         <Route path="/login"  element={<LogInComponent />} ></Route> */}
+         <Route path="/login"  element={<LogInComponent />} ></Route>
          <Route path="/admin" element={<Admin/>}/>
          <Route path="/admin/add-product" element={<AddProduct/>}/>
          <Route path="/admin/remove-product" element={<RemoveProduct/>}/>
