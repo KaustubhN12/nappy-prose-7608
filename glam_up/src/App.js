285efa84f6680ce1ba31b8f4549c3adfae605107
riyashri07 <riyampct7@gmail.com>
2023-02-25 14:35:07 +0530

    footer push

--- glam_up/src/App.js
+++ glam_up/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer/Footer';
-
-
 import Home from './Pages/home';
 //import Makeup_page from './Products/Makeup_page';
 function App() {
   return (
     <div className="App">
       <Home/>
-     {/* <Footer/> */}
+     <Footer/>
     
     </div>
   );
