a1ebcb36c008145b34bb72e31b51b57bd2290a7d
Shvnshgrwl <shvnshgrwl1@gmail.com>
2023-02-25 22:19:20 +0530

    day5

--- glam_up/src/Redux/Reducer.js
+++ glam_up/src/Redux/Reducer.js
@@ -1,4 +1,9 @@
-import { GET_PRODUCT_SUCCESS,PRODUCT_FAILURE, PRODUCT_REQUEST } from "./actionType"
+import { GET_PRODUCT_SUCCESS,PRODUCT_FAILURE, PRODUCT_REQUEST ,cartDataLoading,
+  cartDataSuccess,
+  cartDataError,
+  deleteLoading,
+  deleteSuccess,
+  deleteError,} from "./actionType"
 
 const initialState ={
     isLoading:false,
@@ -15,3 +20,75 @@ export const reducer = (state=initialState, {type,payload}) =>{
     default : return state;
     }
   }
+
+  //.............cart page........
+
+  
+  let initialState1 = {
+    loading: false,
+    data: [],
+    error: false,
+    Totalprice: 0,
+    Totaldiscountprice: 0,
+    ItemCount: 0,
+    deletemsg: "",
+  };
+  
+  export const CartReducer = (state = initialState1, { type, payload }) => {
+    switch (type) {
+      case cartDataLoading: {
+        return {
+          ...state,
+          loading: true,
+        };
+      }
+  
+      case cartDataSuccess: {
+        let beforeDiscoutPrice = 0;
+        let Atdiscountprice = 0;
+        payload.map(({ price, discountedprice }) => {
+          beforeDiscoutPrice += price;
+          Atdiscountprice += discountedprice;
+        });
+        return {
+          ...state,
+          loading: false,
+          data: payload,
+          error: false,
+          ItemCount: payload.length,
+          Totalprice: beforeDiscoutPrice,
+          Totaldiscountprice: Atdiscountprice,
+        };
+      }
+      case cartDataError: {
+        return {
+          loading: false,
+          data: [],
+          error: true,
+        };
+      }
+  
+      case deleteLoading: {
+        return {
+          ...state,
+          loading: true,
+        };
+      }
+      case deleteSuccess: {
+        return {
+          ...state,
+          deletemsg: payload,
+        };
+      }
+      case deleteError: {
+        return {
+          ...state,
+          loading: true,
+          error: true,
+          deletemsg: "",
+        };
+      }
+      default:
+        return state;
+    }
+  };
--- glam_up/src/Redux/store.js
+++ glam_up/src/Redux/store.js
@@ -1,5 +1,7 @@
 import {legacy_createStore , combineReducers, applyMiddleware} from 'redux';
 import {reducer as productReducer} from "./Reducer";
+import { CartReducer } from './CartPage/reducer'
 import thunk from "redux-thunk";
-const rootReducer = combineReducers({productReducer});
+const rootReducer = combineReducers({productReducer,CartReducer});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = legacy_createStore(rootReducer , applyMiddleware(thunk));
